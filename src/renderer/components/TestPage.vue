aa84813017930762130f10267f3bc1a86f5a411e
Christophe Dos Santos <christophe.ajrarn@gmail.com>
2017-12-19 22:07:25 +1100

    feat(Element): Test utilisation du store

--- src/renderer/components/TestPage.vue
+++ src/renderer/components/TestPage.vue
@@ -2,16 +2,32 @@
     <div>
         <h1>Yes it's me</h1>
         <el-rate v-model="value1"></el-rate>
+        <el-button v-on:click="increnentCounter">Compteur</el-button>
+        <h3> {{ count }}</h3>
+        <router-link to="/">Retour accueil</router-link>
     </div>
 
 </template>
 
 <script>
+    import ElButton from 'element-ui/packages/button/src/button'
+
     export default {
+      components: {ElButton},
       name: 'test-page',
       data () {
         return {
           value1: null
+        }
+      },
+      computed: {
+        count () {
+          return this.$store.state.Counter.main
+        }
+      },
+      methods: {
+        increnentCounter () {
+          this.$store.commit('INCREMENT_MAIN_COUNTER')
         }
       }
     }
